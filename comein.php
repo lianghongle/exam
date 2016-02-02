745c955e8cfcf4e43b1047db523058cb7ec1f6f3
lianghongle <lianghongle2012@163.com>
2016-02-02 20:13:53 +0800

    update

--- comein.php
+++ comein.php
@@ -1,27 +1,38 @@
-<?php 
+<?php
 
-    $Static_FilePath="static/";
-   // echo $_SERVER["REQUEST_URI"];
-    $get_path=str_replace("/","",$_SERVER["REQUEST_URI"]);
+//定义路径分隔符
+defined('DS') || define('DS', DIRECTORY_SEPARATOR);
+
+//定义静态文件目录
+//$static_filePath = "static/";// echo $_SERVER["REQUEST_URI"];
+defined('STATIC_PATH') || define('STATIC_PATH', 'static' . DS);
+defined('STATIC_COMMON_PATH') || define('STATIC_COMMON_PATH', 'static' . DS . 'common' . DS);
+
+defined('API_PATH') || define('API_PATH', 'api' . DS );
+
+//请求路径
+$get_path = str_replace("/", "", $_SERVER["REQUEST_URI"]);
+
+if (file_exists(STATIC_PATH . $get_path)) {//进入静态资源
 	
-	if(file_exists($Static_FilePath.$get_path))
-	{
-		include($Static_FilePath.$get_path);//进入静态资源
-	}
-	else 
-	{
-		if(file_exists("api/".$get_path.".service")) // 加载 controller
-		{
-			require("functions");
-			require("api/".$get_path.".service");
-			$class_name=$get_path."Service";
-			$loadClass=new $class_name();
-			$loadClass->run();
-		}
-	}
+	//加载header.html
+	include (STATIC_COMMON_PATH . 'header.html');
 	
-    
+	include (STATIC_PATH . $get_path);
 	
-    
-     
-?>
+	//加载footer.html
+	include (STATIC_COMMON_PATH . 'footer.html');
+} else if(file_exists(API_PATH . $get_path . ".service")) {// 加载 controller
+
+		//公用function
+		require ("functions");
+		
+		require (API_PATH . $get_path . ".service");
+		
+		$class_name = ucfirst($get_path) . "Service";
+//		$loadClass = new $class_name();
+//		$loadClass -> run();
+		(new $class_name())->run();
+} else {
+	echo '404';
+}
