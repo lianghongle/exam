18300893627712f43ac01ea1138cdbf6676d7023
lianghongle <lianghongle2012@163.com>
2016-02-03 19:28:40 +0800

    update 2016-2-3 19:28:22

--- comein.php
+++ comein.php
@@ -1,5 +1,9 @@
 <?php
 
+//开发，开启报错
+error_reporting(-1);
+ini_set('display_errors', 1);
+
 //定义路径分隔符
 defined('DS') || define('DS', DIRECTORY_SEPARATOR);
 
@@ -10,26 +14,34 @@ defined('STATIC_COMMON_PATH') || define('STATIC_COMMON_PATH', 'static' . DS . 'c
 
 defined('API_PATH') || define('API_PATH', 'api' . DS );
 
-//请求路径
-$get_path = str_replace("/", "", $_SERVER["REQUEST_URI"]);
+//请求路径 http://192.168.10.128:9090/user?login_mobile=&login_pwd=
+//$get_path = str_replace("/", "", $_SERVER["REQUEST_URI"]);
+//var_dump($_SERVER);die;
 
-if (file_exists(STATIC_PATH . $get_path)) {//进入静态资源
+$request_uri = isset($_SERVER["REQUEST_URI"]) ? str_replace("/", "", $_SERVER["REQUEST_URI"]) : '';// /user?login_mobile=&login_pwd=
+$script_name = str_replace("/", "", $_SERVER["SCRIPT_NAME"]);// /user
+$query = isset($_SERVER["QUERY_STRING"]) ? str_replace("/", "", $_SERVER["QUERY_STRING"]) : '';// login_mobile=&login_pwd=
+
+$service = $script_name;
+
+if (file_exists(STATIC_PATH . $service)) {//进入静态资源
 	
 	//加载header.html
 	include (STATIC_COMMON_PATH . 'header.html');
 	
-	include (STATIC_PATH . $get_path);
+	include (STATIC_PATH . $service);
 	
 	//加载footer.html
 	include (STATIC_COMMON_PATH . 'footer.html');
-} else if(file_exists(API_PATH . $get_path . ".service")) {// 加载 controller
+} else if(file_exists(API_PATH . $service . ".service")) {// 加载 controller
 
 		//公用function
 		require ("functions");
 		
-		require (API_PATH . $get_path . ".service");
+		require (API_PATH . "base.service");
+		require (API_PATH . $service . ".service");
 		
-		$class_name = ucfirst($get_path) . "Service";
+		$class_name = ucfirst($service) . "Service";
 //		$loadClass = new $class_name();
 //		$loadClass -> run();
 		(new $class_name())->run();
